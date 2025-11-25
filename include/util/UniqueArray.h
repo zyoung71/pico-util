c36db286fb8fa850ebe06e9835221ffe50ead6b6
zyoung71 <zyoung0071@gmail.com>
2025-11-24 22:02:27 -0500

    make_unique_array

--- include/util/UniqueArray.h
+++ include/util/UniqueArray.h
@@ -12,3 +12,21 @@ struct UniqueArray
         return array[get];
     }
 };
+
+template<typename T, size_t l>
+inline constexpr UniqueArray<T> make_unique_array(T (arr)[l])
+{
+    return UniqueArray<T>{
+        std::make_unique<T[]>(arr),
+        len
+    };
+}
+
+template<typename T>
+inline constexpr UniqueArray<T> make_unique_array(std::unique_ptr<T[]>& arr, size_t len)
+{
+    return UniqueArray<T>{
+        std::move(arr),
+        len
+    };
+}
